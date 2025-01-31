dcfa4bec38dde5f364ea250185000b321e4e4350
Pedro Ferreira <prgf2011@gmail.com>
2025-01-31 19:15:49 +0000

    Missing speech mark!!

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:9090/api";
-const baseURL = "http://104.248.94.211:9090/api;
+const baseURL = "http://104.248.94.211:9090/api";
 
 const db = axios.create({
   baseURL: baseURL,
